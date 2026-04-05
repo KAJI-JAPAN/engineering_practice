f4d94ee24b0c279515a3cc9c7ea896371aa77e7d
KAJI-JAPAN <kurowassan1206@gmail.com>
2026-04-06 00:36:06 +0900

    practice

--- ex/20260405/1.rb
+++ ex/20260405/1.rb
@@ -19,29 +19,42 @@ end
 
 class OrdersController < ApplicationController
   def create
-    @current_cart = Cart.new(current_user)
-    redirect_to cart_path, alert: "カートが空です" if current_cart
+    cart = Cart.new(current_user)
 
-    @current_cart.order_items
-    redirect_to @order, notice: "注文が完了しました"
+    if (@order = cart.order_items)
+      redirect_to @order, notice: "注文が完了しました"
+    else
+      redirect_to cart_path, alert: "カートが空です"
+    end
   end
 end
 
-class Cart < ActiveRecord::Base
-  def initialize(current_user)
-    if current_user.cart.items.any? && current_user.cart.total_price < 0
-      raise  "カートが空です"
-    end
+class Cart
 
-    @current_user = current_user
+  attr_reader :user
 
+  def initialize(user)
+    @user = user
     self.freeze
-
   end
 
   def order_items
-    Order.create!(user: current_user, amount: current_user.cart.total_price)
-    current_user.cart.items.destroy_all
+    nil unless can_checkout?
+
+    Order.transaction do
+      order = Order.create!(user: user, amount: total_price)
+      user.cart_items.destroy_all
+      order
+    end
+  end
+
+  private
+  def can_checkout?
+    user.cart_items.any? && total_price > 0
+  end
+
+  def total_price
+    user.cart_items.sum(:price)
   end
 end
 
